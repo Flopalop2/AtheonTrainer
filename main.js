b44193634c32e53d00f9780eb72e5261f8142d6c
Michael Brown <69126427+Flopalop2@users.noreply.github.com>
2024-03-03 22:00:09 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,21 @@
 window.addEventListener("keypress", function(event) {
   switch(event.key) {
     case "1":
-      event.preventDefault();
       document.getElementById("btn1").click();
       break;
     case "2":
-      event.preventDefault();
       document.getElementById("btn2").click();
       break;
     case "3":
-      event.preventDefault();
       document.getElementById("btn3").click();
       break;
     case "4":
-      event.preventDefault();
       document.getElementById("btn4").click();
       break;
     case "5":
-      event.preventDefault();
       document.getElementById("btn5").click();
       break;
     case "6":
-      event.preventDefault();
       document.getElementById("btn6").click();
       break;
     case "r":
