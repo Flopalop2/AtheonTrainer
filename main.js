12cc5a45e2ef3fdd39f57c911b1b65b657335ca8
Michael Brown <69126427+Flopalop2@users.noreply.github.com>
2024-03-03 22:17:08 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,41 @@
+var G=4;
+var F=5;
+var E=6;
+var A=1;
+var C=2;
+var D=3;
+
 const queryString = window.location.search;
 console.log(queryString);
+const urlParams = new URLSearchParams(queryString);
+if (urlParams != null) {
+  G=urlParams.get('G');
+  F=urlParams.get('F');
+  E=urlParams.get('E');
+  A=urlParams.get('A');
+  C=urlParams.get('C');
+  D=urlParams.get('D');
+}
 
 window.addEventListener("keypress", function(event) {
   switch(event.key) {
     case "1":
-      document.getElementById("btn1").click();
+      document.getElementById(A).click();
       break;
     case "2":
-      document.getElementById("btn2").click();
+      document.getElementById(C).click();
       break;
     case "3":
-      document.getElementById("btn3").click();
+      document.getElementById(D).click();
       break;
     case "4":
-      document.getElementById("btn4").click();
+      document.getElementById(G).click();
       break;
     case "5":
-      document.getElementById("btn5").click();
+      document.getElementById(F).click();
       break;
     case "6":
-      document.getElementById("btn6").click();
+      document.getElementById(E).click();
       break;
     case "r":
       event.preventDefault();
